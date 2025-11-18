6a7805016fde422d028a0730436c36d806510de8
weasdf12 <jjyyjj118@naver.com>
2025-11-18 19:33:03 +0900

    외화통장 update

--- flobank-api/src/main/java/kr/co/api/flobankapi/service/TermsDbService.java
+++ flobank-api/src/main/java/kr/co/api/flobankapi/service/TermsDbService.java
@@ -198,12 +198,4 @@ public class TermsDbService {
         return list;
     }
 
-
-
-    public List<TermsHistDTO> getTermsByLocation(int termCate) {
-        log.info("[고객 약관 조회] cate={}", termCate);
-        List<TermsHistDTO> list = mapper.selectTermsByCate(termCate);
-        log.info("[고객 약관 조회] 총 {}건", list.size());
-        return list;
-    }
 }
